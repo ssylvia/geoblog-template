eed200db7889147ede5c57669058ca6dd491c82e
Stephen Sylvia <ssylvia@esri.com>
2013-09-24 12:54:39 -0400

    update config

--- app/Config.js
+++ app/Config.js
@@ -5,9 +5,9 @@ define([],
 			//The appid for the configured application
 			appid: "",
 			//The web map id
-			webmap: "201c9b57132c426ca7236a885b91959e",
+			webmap: "2406f3f2e13e49539312cd02cae22da7",
 			//ArcGIS Online usernames of those who are authorized the edit blog, sepereate by comma
-			authorizedEditors: ["StoryMaps","ssylvia_stories","acarroll_storymaps","dasbury_storymaps"],
+			authorizedEditors: ["StoryMaps","ssylvia_stories","acarroll_storymaps","dasbury_storymaps","rrose_story"],
 			//Enter the url to the feature service storing blog posts
 			featureService: "http://services.arcgis.com/nzS0F0zdNLvs7nc8/arcgis/rest/services/Elephants/FeatureServer/0",
 			//Start post object ID
@@ -19,7 +19,7 @@ define([],
 			//Feature servide field to sort by
 			sortBy: "time",
 			//Display order of feature service; specfiy "ASC" (ascending) or "DESC" (descending).
-			order: "DESC",
+			order: "ASC",
 			//Number of post available per pages (recommend 10 for smaller screens);
 			postsPerPage: 10,
 			//If true, time extent is cumulative to blog post date. If false, time extent is the state in time of blog post
@@ -29,9 +29,9 @@ define([],
 			//If true, when a blog post is selected and a point has been set on the map, the point graphic will display even if layer is turned off
 			alwaysDisplayPoints: false,
 			//Enter a title, if no title is specified, the webmap's title is used.
-			title: "",
+			title: "Devastating decline of forest elephant habitat in Central Africa",
 			//Enter a subtitle, if not specified the ArcGIS.com web map's summary is used
-			subtitle: "",
+			subtitle: "To save the remaining African forest elephants, illegal poaching for ivory and encroachment into core elephant habitat must be stopped. In addition, the international demand for ivory, which fuels illegal trade, must be dramatically reduced.",
 			//Enter URL to be used with social media buttons embedded in post. The URL will also be appended with the post object id so make sure query parameter is included
 			socialURL: "",
 			//If the webmap uses Bing Maps data, you will need to provided your Bing Maps Key
